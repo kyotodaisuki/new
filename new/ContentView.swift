e9f5ed17e94b40e40db19b4f5dd3646828a78e80
mmyang <dongbala74@gmail.com>
2024-05-09 13:05:20 +0900

    Initial Commit

---
+++ new/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  new
+//
+//  Created by m m yang on 2024/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by m m yang on 2024/05/09.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
